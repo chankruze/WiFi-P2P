7406b05baad25c7dd257eaa1cc44b93226767f4c
chankruze <chankruze@gmail.com>
2019-05-12 15:08:07 +0530

    Implement WiFi Manager
    Add functionality to toggle wifi button to turn wifi on & off.
    Set button text programatically then using XML hard coded android:text.
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/src/main/java/in/geekofia/wifip2p/MainActivity.java
+++ app/src/main/java/in/geekofia/wifip2p/MainActivity.java
@@ -1,12 +1,16 @@
 package in.geekofia.wifip2p;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     ListView listPeers;
     TextView readMessages, connectionStats;
     EditText writeMessage;
+    WifiManager mWifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +27,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initUi();
+        wifiToggleButton();
+    }
+
+    private void wifiToggleButton() {
+        btnWifiToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mWifiManager.isWifiEnabled()){
+                    Toast.makeText(getApplicationContext(),"Turning Off WiFi ...", Toast.LENGTH_SHORT).show();
+                    btnWifiToggle.setText("Turn WiFi On");
+                    mWifiManager.setWifiEnabled(false);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Turning On WiFi ...", Toast.LENGTH_SHORT).show();
+                    btnWifiToggle.setText("Turn WiFi Off");
+                    mWifiManager.setWifiEnabled(true);
+                }
+            }
+        });
     }
 
     private void initUi() {
+        mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
         btnWifiToggle = (Button) findViewById(R.id.wifiToggle);
-        btnWifiToggle.setText("Turn WiFi On");
+
+        if(mWifiManager.isWifiEnabled()){
+            Toast.makeText(getApplicationContext(),"WiFi is already enabled", Toast.LENGTH_LONG).show();
+            btnWifiToggle.setText("Turn WiFi Off");
+        }else {
+            Toast.makeText(getApplicationContext(),"Turn On WiFi", Toast.LENGTH_LONG).show();
+            btnWifiToggle.setText("Turn WiFi Off");
+            mWifiManager.setWifiEnabled(true);
+        }
 
         btnDiscover = (Button) findViewById(R.id.discoverPeers);
         btnDiscover.setText("Discover Peers");
